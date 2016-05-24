7ac61359dd8acd010a26af9f0f98c622514917f2
Brian Leathem <bleathem@gmail.com>
2016-05-24 16:05:07 -0700

    Re-factored the click listener to use a single listener on the game container

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 'use strict';
 
-var balls = document.querySelectorAll('.ball');
+var gameContainer = document.querySelector('.game-container');
 var scoreboard = document.querySelector('.scoreboard');
 var scoreElement = scoreboard.querySelector('h1');
 var score = 0;
 scoreElement.innerText = score;
 
-Array.from(balls).forEach(ball => {
-  ball.addEventListener('click', function(event) {
+gameContainer.addEventListener('click', function(event) {
+  if (event.srcElement.classList.contains('ball')) {
+    var ball = event.srcElement;
     scoreElement.innerText = ++score;
     ball.parentElement.removeChild(ball);
-  });
+  }
 });
