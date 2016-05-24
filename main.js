46389f8f81e59203fbe2762748e2cd5e7e002b99
Brian Leathem <bleathem@gmail.com>
2016-05-24 16:23:50 -0700

    Added the ball back after removing it

--- main.js
+++ main.js
@@ -6,10 +6,20 @@ var scoreElement = scoreboard.querySelector('h1');
 var score = 0;
 scoreElement.innerText = score;
 
+function addBall(line, ball) {
+  setTimeout(function() {
+    var newLine = line.cloneNode(true);
+    newLine.querySelector('.ball-container').appendChild(ball);
+    line.parentElement.replaceChild(newLine, line);
+  }, 1000)
+}
+
 gameContainer.addEventListener('click', function(event) {
   if (event.srcElement.classList.contains('ball')) {
     var ball = event.srcElement;
+    var line = ball.parentElement.parentElement;
     scoreElement.innerText = ++score;
     ball.parentElement.removeChild(ball);
+    addBall(line, ball);
   }
 });
