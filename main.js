7186144504cd8fde5535f34f2f29f72dbd27c136
Brian Leathem <bleathem@gmail.com>
2016-05-25 16:19:14 -0700

    Re-factored an "anim" CSS class out of line.  Toggled that class to enable/disable animation

--- main.js
+++ main.js
@@ -3,32 +3,56 @@
   var gameContainer = document.querySelector('.game-container'),
       scoreboard = document.querySelector('.scoreboard'),
       hitsElement = scoreboard.querySelector('h1 .number'),
-      missesElement = scoreboard.querySelector('h2 .number'),
+      missesElement = scoreboard.querySelectorAll('h2 .number').item(0),
+      escapesElement = scoreboard.querySelectorAll('h2 .number').item(1),
       hits = 0,
-      misses = 0;
+      misses = 0,
+      escapes = 0;
   hitsElement.innerText = hits,
   missesElement.innerText = misses;
+  escapesElement.innerText = escapes;
 
-  function addBall(line, container) {
+  function addBall(line, container, delay) {
     setTimeout(function() {
-      var newLine = line.cloneNode(true);
-      newLine.appendChild(container);
-      var animationDuration = 0.75 * parseFloat(newLine.style.animationDuration);
-      newLine.style.animationDuration = animationDuration + 's';
+      line.classList.remove('anim');
+      line.appendChild(container);
+      var animationDuration = 0.75 * parseFloat(line.style.animationDuration);
+      animationDuration = Math.max(animationDuration, 0.5);
+      line.style.animationDuration = animationDuration + 's';
+      requestAnimationFrame(function() {
+        line.classList.add('anim');
+      }, 0);
+    }, delay)
+  }
 
-      line.parentElement.replaceChild(newLine, line);
-    }, 100)
+  function removeBall(line) {
+    var container = line.querySelector('.ball-container');
+    line.removeChild(container);
+    line.classList.remove('anim');
+    return container;
   }
 
   gameContainer.addEventListener('click', function(event) {
     if (event.srcElement.classList.contains('ball')) {
-      var container = event.srcElement.parentElement;
-      var line = container.parentElement;
+      var line = event.srcElement.parentElement.parentElement;
+      var container = removeBall(line);
       hitsElement.innerText = ++hits;
-      line.removeChild(container);
-      addBall(line, container);
+      addBall(line, container, 100);
     } else {
       missesElement.innerText = ++misses;
     }
   });
+
+  gameContainer.addEventListener('animationiteration', function(event) {
+    if (event.target.classList.contains('line')) {
+      var line = event.target;
+      var container = removeBall(line);
+      escapesElement.innerText = ++escapes;
+      addBall(line, container, 1000);
+    }
+  });
+
+  Array.from(gameContainer.querySelectorAll('.line')).forEach(function(line) {
+    line.classList.add('anim');
+  })
 }());
