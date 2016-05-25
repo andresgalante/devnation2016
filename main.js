045e8cbbceadb4f45e7889c8e476d919fa420cdc
Brian Leathem <bleathem@gmail.com>
2016-05-25 15:34:42 -0700

    Removed/Added the ball-container, rather than the ball

--- main.js
+++ main.js
@@ -9,21 +9,21 @@
   hitsElement.innerText = hits,
   missesElement.innerText = misses;
 
-  function addBall(line, ball) {
+  function addBall(line, container) {
     setTimeout(function() {
       var newLine = line.cloneNode(true);
-      newLine.querySelector('.ball-container').appendChild(ball);
+      newLine.appendChild(container);
       line.parentElement.replaceChild(newLine, line);
     }, 1000)
   }
 
   gameContainer.addEventListener('click', function(event) {
     if (event.srcElement.classList.contains('ball')) {
-      var ball = event.srcElement;
-      var line = ball.parentElement.parentElement;
+      var container = event.srcElement.parentElement;
+      var line = container.parentElement;
       hitsElement.innerText = ++hits;
-      ball.parentElement.removeChild(ball);
-      addBall(line, ball);
+      line.removeChild(container);
+      addBall(line, container);
     } else {
       missesElement.innerText = ++ misses;
     }
